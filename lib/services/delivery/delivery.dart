46e72ae3716fff84913bc5311e765fa6476801b6
fshangala <fshangala@gmail.com>
2023-09-12 20:03:22 +0200

    minor changes made to the app

--- lib/services/delivery/delivery.dart
+++ lib/services/delivery/delivery.dart
@@ -1,6 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
 
 AppBar deliveryAppBar = AppBar(title: const Text('Uthenge - Delivery Service'));
 
